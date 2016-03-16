64262cbfbeb560efe2042222a6f06f2f7e471b90
zhenghuijie <13007568302@163.com>
2016-03-16 10:12:19 +0800

    add ##

---
+++ sharp/double_sharp.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void quit_command()
+{
+	printf("I am quit command\n");
+}
+
+void help_command()
+{
+	printf("I am help command\n");
+}
+
+struct command
+{
+	char *name;
+	void (*function)(void);	
+};
+
+#define COMMAND(NAME) {#NAME, NAME##_command}
+#define PRINT(NAME) printf("token"#NAME"=%d\n", token##NAME);
+
+int main(int argc, char **argv)
+{
+	int token9=9;
+	PRINT(9);
+	struct command commands[] = 
+	{
+		COMMAND(quit),
+		COMMAND(help),
+	};
+	commands[0].function();
+	return 0;
+}
