99986dc5ff68b120fe005ec7d752ee75d2ef5250
zhenghuijie <13007568302@163.com>
2016-04-07 14:30:52 +0800

    update wide char

--- wide_char/swprintf.c
+++ wide_char/swprintf.c
@@ -3,9 +3,11 @@
 
 int main(void)
 {
-	wchar_t wch[64];
+	wchar_t wch[64], wstr[128];
 	swprintf(wch, sizeof(wch) / sizeof(wch[0]), 
-			L"%s", "zhj");
-	wprintf(L"%ls\n", wch);
+				L"%s", "zhj");
+	swprintf(wstr, sizeof(wstr) / sizeof(wstr[0]), 
+			L"%ls%s", wch, " is here");
+	wprintf(L"%ls\n", wstr);
 	return 0;
 }
