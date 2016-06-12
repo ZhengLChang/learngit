c1a04b0bfa8891350c9fb9cac18b83a69861e9c3
zhenghuijie <13007568302@163.com>
2016-06-12 18:16:08 +0800

    add test_git

---
+++ switch/switch.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	int c = 1;
+	switch(c)
+	{
+		default:
+			printf("default^0^\n");
+			break;
+		case 1:
+			printf("1 ^_^\n");
+			break;
+	}
+	return 0;
+}
